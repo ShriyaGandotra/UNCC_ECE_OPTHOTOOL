2bdba49360f49facc49fb01e4bf46147720ac29c
Shriya Gandotra <106940773+ShriyaGandotra@users.noreply.github.com>
2023-10-06 17:09:09 -0400

    Add files via upload

---
+++ Front End/opthotool_front_end.py
@@ -0,0 +1,32 @@
+#------------- LIBRARIES ------------#
+
+
+#-------------- BUTTON --------------#
+# Pop up page 
+# Integrate Machine Learning Predictions - Positve or negative
+# Integrate Condifence Score prediction
+
+#------------ UPLOAD FILE -----------#
+
+
+#------------ SELECT SCAN -----------#
+
+
+#------------ STORE SCAN ------------#
+## AWS EC2 instance - database (Scalable)
+## For testing use local storing
+
+#------------ NEW WEBPAGE ------------#
+
+
+#-- IDENTIFICATION OF RETINAL LAYER --#
+# Integrate Artrey segmentation 
+# Integrate vein segmentation 
+
+
+
+
+
+
+
+
