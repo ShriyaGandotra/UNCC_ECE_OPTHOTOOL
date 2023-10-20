6aed9a7de3fca769be07378a0e1fce41f966bb20
Adam Shigapov <ashigapo@uncc.edu>
2023-10-20 16:49:35 -0400

    Preliminary B-Scan Segmenter, it works, but the image quality is very important when getting a proper segmentation.

---
+++ Backend/Segmenter.py
@@ -0,0 +1,53 @@
+# Libraries
+import numpy as np
+import os, cv2, glob
+from keras.utils import normalize
+from matplotlib import pyplot as plt
+import tensorflow as tf
+
+IMG_SIZE = 640
+
+# Capture training image info as a list
+test_test = []
+test_path = '/Users/adams/Desktop/OCT-Segmenter/Test'
+
+for directory_path in glob.glob(test_path):
+    for img_path in glob.glob(os.path.join(directory_path, "*.jpeg")):
+        img = cv2.imread(img_path, 0)
+        img = cv2.resize(img, (640, 640))
+        test_test.append(img)
+        print(img_path)
+
+# Convert list to array for machine learning processing
+test_test = np.array(test_test)
+# test_test = test_test.reshape(-1, IMG_SIZE, IMG_SIZE)
+
+plt.figure(figsize=(40,100))
+for i in range(8):
+    plt.subplot(14, 5, i+1)
+    plt.imshow(test_test[i,:,:])
+    plt.title("(Label: " + str(i) + ")")
+plt.show()
+
+
+test_test = np.expand_dims(test_test, axis=3)
+test_test = normalize(test_test, axis=1)
+
+model_location = '/Users/adams/Downloads/retina_segmentation_8_layer.hdf5'
+model = tf.keras.models.load_model(model_location)
+# model.summary()
+print(test_test.shape)
+
+test1 = test_test[7]
+test_img_norm = test1[:, :, 0][:, :, None]
+test = np.expand_dims(test_img_norm, 0)
+
+prediction = (model.predict(test))
+predicted_img = np.argmax(prediction, axis=3)[0, :, :]
+print(prediction.shape)
+
+
+plt.figure(figsize=(40, 20))
+plt.title('Prediction<')
+plt.imshow(predicted_img, cmap='jet')
+plt.show()
--- Backend/oct-converter.py
+++ Backend/oct-converter.py
@@ -1,59 +1,66 @@
 import json
 
-filepath = ""
+filepath = ''
 
-#convert .fds files to png
+
+# convert .fds files to png
 def oct_converter_fds(filepath):
     from oct_converter.readers import FDS
     file = FDS(filepath)
     oct_volumes = (file.read_oct_volume())
     oct_volumes.save("image.png")
 
-#convert .fda files to png
+
+# convert .fda files to png
 def oct_converter_fda(filepath):
     from oct_converter.readers import FDA
     file = FDA(filepath)
     oct_volumes = (file.read_oct_volume())
     oct_volumes.save("image.png")
 
-#conversion of e2e files into png images
+
+# conversion of e2e files into png images
 def oct_converter_e2e(filepath):
     from oct_converter.readers import E2E
     file = E2E(filepath)
     oct_volumes = (file.read_oct_volume())
-    
+
     for volume in oct_volumes:
         oct_volumes.peek(show_contours=True)
         volume.save("image.png")
 
-#convert .img files to png
+
+# convert .img files to png
 def oct_converter_img(filepath):
     from oct_converter.readers import IMG
     file = IMG(filepath)
     oct_volume = (file.read_oct_volume())
     oct_volume.save("image.png")
 
-#conversion of BOCT(bioptigen) files into png images
+
+# conversion of BOCT(bioptigen) files into png images
 def oct_converter_boct(filepath):
     from oct_converter.readers import BOCT
     file = BOCT(filepath)
     oct_volumes = file.read_oct_volume()
-    
+
     for oct in oct_volumes:
         oct_volumes.peek(show_contours=True)
         oct.save("image.png")
-        
-#conversion of POCT(optovue) files into png images
+
+
+# conversion of POCT(optovue) files into png images
 def oct_converter_poct(filepath):
     from oct_converter.readers import POCT
     file = POCT(filepath)
     oct_volumes = file.read_oct_volume()
-    
+
     for volume in oct_volumes:
         oct_volumes.peek(show_contours=True)
         volume.save("image.png")
 
-#convert .dcm files to png
+
+# convert .dcm files to png
 def oct_converter_dcm(filepath):
     from oct_converter.readers import Dicom
     file = Dicom(filepath)
