f036ec7ef55d77d7cb0069de9f4436378a99a51e
Lauren Bourque <lolobq47@outlook.com>
2023-11-22 16:10:58 -0500

    Update layer_measurement_tool.py

--- Backend/layer_measurement_tool.py
+++ Backend/layer_measurement_tool.py
@@ -5,6 +5,8 @@
 from PIL import Image
 import os
 import pandas as pd
+import colorir.color_class
+import numpy as np
 
 
 def run_code():
@@ -13,10 +15,10 @@ def run_code():
     """
     image: Image = get_image()
     print(image)
-    layer_data: list[pd.DataFrame] = get_layer_data(image_in=image)
-    print(layer_data[0].head())
+    layer_data: dict[pd.DataFrame] = get_layer_data(image_in=image)
     all_layer_thicknesses: list[pd.Series] = calculate_thicknesses(layers_in=layer_data, image_in=image)
     avg_thicknesses: list[int] = calculate_avg_thicknesses(layer_thicknesses_in=all_layer_thicknesses)
+    print(avg_thicknesses)
 
 
 def get_image():
@@ -39,44 +41,61 @@ def get_layer_data(image_in: Image):
     """
     # List of RGB colors to get pixel data from
     colors = [
-        (228, 255, 18),  # Color 1 (Yellow)
-        (21, 255, 225),  # Color 2 (Teal)
-        (255, 148, 0),  # Color 3 (Orange)
-        (0, 0, 255),  # Color 4 (Darker Blue)
-        (255, 29, 0),  # Color 5 (Red)
-        (124, 255, 121),  # Color 6 (Green)
-        (0, 128, 255),  # Color 7 (Sky Blue)
-        (127, 0, 0),  # Color 8 (Burgundy)
+        '#00007f',  # Background color (Dark Blue)
+        '#e4ff12',  # Color 1 (Yellow)
+        '#15ffe1',  # Color 2 (Teal)
+        '#ff9400',  # Color 3 (Orange)
+        '#0028ff',  # Color 4 (Darker Blue)
+        '#ff1d00',  # Color 5 (Red)
+        '#7cff79',  # Color 6 (Green)
+        '#0080ff',  # Color 7 (Sky Blue)
+        '#7f0000',  # Color 8 (Burgundy)
     ]
 
-    # List of dataframes
-    layers = []
+    # Dictionary of lists (Starts with Color 1 [Yellow])
+    layers = {hex_color: [] for hex_color in colors[1:]}
 
     width, height = image_in.size
 
-    for color in colors:
-        # Lists to store the x and y coordinates
-        x_coordinates = []
-        y_coordinates = []
+    # Convert the image to a NumPy array for faster processing
+    image_array = np.array(image_in)
 
-        # Loop through the pixels
-        for y in range(height):
-            for x in range(width):
-                # Get the RGB value of the current pixel
-                pixel_value = image_in.getpixel((x, y))
+    # Loop through the pixels
+    for y in range(height):
+        for x in range(width):
+            # Get the RGB value of the current pixel and convert to hex
+            pixel_value = image_array[y, x]
+            hex_value = rgb_to_hex(pixel_value)
+            closest_color = calculate_hue(target=hex_value, tints=colors)
 
-                if pixel_value == color:  # RGB Color
-                    x_coordinates.append(x)
-                    y_coordinates.append(y)
+            # Make sure the color isn't the background color
+            if closest_color is not None and closest_color != colors[0]:  # Hex Color
+                layers[closest_color].append({'x': x, 'y': y})
 
-        # Create the dataframe for the layer and add it to the list of layers
-        pixel_df = pd.DataFrame({'x': x_coordinates, 'y': y_coordinates})
-        layers.append(pixel_df)
+    # Convert lists of dictionaries to DataFrames
+    layers = {color: pd.DataFrame(data) for color, data in layers.items()}
 
     return layers
 
 
-def calculate_thicknesses(layers_in: list[pd.DataFrame], image_in: Image):
+def rgb_to_hex(rgb):
+    # Convert RGB tuple to hexadecimal color code
+    hex_color = "#{:02x}{:02x}{:02x}".format(*rgb)
+    return hex_color
+
+
+def calculate_hue(target, tints):
+    target_hue = colorir.color_class.HexRGB(target).hsl()[0]
+    closest = (None, 180)
+    for tint in tints:
+        hue = colorir.color_class.HexRGB(tint).hsl()[0]
+        hue_dist = (min(target_hue, hue) - max(target_hue, hue)) % 360
+        if hue_dist < closest[1]:
+            closest = (tint, hue_dist)
+    return closest[0]
+
+
+def calculate_thicknesses(layers_in: dict[pd.DataFrame], image_in: Image):
     """
     Stores the x and y coordinates of the different pixel values in each layer. Creates a dataframe for each color (layer)
     :param layers_in (list of DataFrame) -> the list of DataFrames representing each layer
@@ -89,7 +108,7 @@ def calculate_thicknesses(layers_in: list[pd.DataFrame], image_in: Image):
     all_layer_thicknesses: list = []
 
     # Loop through the Dataframes
-    for layer in layers_in:
+    for key, layer in layers_in.items():
         # Create a Pandas Series called layer_thicknesses to store the thicknesses for one layer
         layer_thicknesses = pd.Series(name='layer_thicknesses')
 
@@ -99,19 +118,52 @@ def calculate_thicknesses(layers_in: list[pd.DataFrame], image_in: Image):
 
             if not subset_layer.empty:
                 # Find the biggest and smallest y values
-                biggest_y = subset_layer['y'].max()
-                smallest_y = subset_layer['y'].min()
+                biggest_y = find_max_y_val(subset_in=subset_layer)
+                smallest_y = find_min_y_val(subset_in=subset_layer)
 
-                # Calculate the difference
-                thickness = biggest_y - smallest_y
+                if biggest_y is not None and smallest_y is not None:
+                    # Calculate the difference
+                    thickness = biggest_y - smallest_y + 1
 
-                # Append the value to the Series
-                layer_thicknesses[x_val] = thickness
+                    # Append the value to the Series
+                    layer_thicknesses[x_val] = thickness
 
         # Add the thickness values for the one layer to the list of layer thicknesses
         all_layer_thicknesses.append(layer_thicknesses)
 
     return all_layer_thicknesses
+
+
+def find_min_y_val(subset_in: pd.DataFrame):
+    # Sort values in the 'y' column from least to greatest
+    subset_sorted = subset_in.sort_values(by='y')
+
+    # Iterate through the sorted values using the index
+    for index, value in subset_sorted.iterrows():
+        current_value = value['y']
+
+        # Check if there is a value 1 greater than the current value
+        has_one_greater = (subset_sorted['y'] == current_value + 1).any()
+
+        # If there is, return the current value
+        if has_one_greater:
+            return current_value
+
+
+def find_max_y_val(subset_in: pd.DataFrame):
+    # Sort values in the 'y' column from greatest to least
+    subset_sorted = subset_in.sort_values(by='y', ascending=False)
+
+    # Iterate through the sorted values using the index
+    for index, value in subset_sorted.iterrows():
+        current_value = value['y']
+
+        # Check if there is a value 1 greater than the current value
+        has_one_less = (subset_sorted['y'] == current_value - 1).any()
+
+        # If there is, return the current value
+        if has_one_less:
+            return current_value
 
 
 def calculate_avg_thicknesses(layer_thicknesses_in: list[pd.Series]):
