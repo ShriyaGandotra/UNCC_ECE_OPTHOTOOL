04d2266b0c5c28915ed91b4bc14476e2fd380a8c
YousefAbua <143196892+YousefAbua@users.noreply.github.com>
2024-02-13 15:51:45 -0500

    Update distance_tool.py

--- Backend/distance_tool.py
+++ Backend/distance_tool.py
@@ -7,27 +7,28 @@ import math
 from PIL import Image, ImageTk
 
 # Global variables to store point coordinates and line
-IMAGE_DIM = 6 #6mm x 6mm assumption
-height = 0 #height per pixel
-width = 0 #width per pixel
+IMAGE_DIM = 6  # 6mm x 6mm assumption
+height = 0  # height per pixel
+width = 0  # width per pixel
 point1 = None
 point2 = None
 point1_oval = None
 point2_oval = None
 line = None
 measure_mode = False
+image_loaded = False
 
 def calc_image_size(pixel_width, pixel_height):
     global width, height, IMAGE_DIM
 
-    width = IMAGE_DIM/pixel_width
-    height = IMAGE_DIM/pixel_height
+    width = IMAGE_DIM / pixel_width
+    height = IMAGE_DIM / pixel_height
 
 # Function to calculate the distance
 def calculate_distance():
     global point1, point2, line
     if point1 and point2:
-        distance = math.sqrt(((point2[0] - point1[0])*width)**2 + ((point2[1] - point1[1])*height)**2)
+        distance = math.sqrt(((point2[0] - point1[0]) * width) ** 2 + ((point2[1] - point1[1]) * height) ** 2)
         result_label.config(text=f"Distance: {distance:.2f} mm")
         # Draw a line between the two points
         if line:
@@ -37,24 +38,27 @@ def calculate_distance():
 # Function to set the selected point
 def set_point(event):
     global point1, point2, point1_oval, point2_oval
-    if point1 is None:
-        point1 = (event.x, event.y)
-        point1_oval = canvas.create_oval(event.x - 5, event.y - 5, event.x + 5, event.y + 5, outline="red", width=0.5, fill="red")
-    elif point2 is None:
-        point2 = (event.x, event.y)
-        point2_oval = canvas.create_oval(event.x - 5, event.y - 5, event.x + 5, event.y + 5, outline="red", width=0.5, fill="red")
-        calculate_distance()
+    if image_loaded:
+        if point1 is None:
+            point1 = (event.x, event.y)
+            point1_oval = canvas.create_oval(event.x - 5, event.y - 5, event.x + 5, event.y + 5, outline="red",
+                                              width=0.5, fill="red")
+        elif point2 is None:
+            point2 = (event.x, event.y)
+            point2_oval = canvas.create_oval(event.x - 5, event.y - 5, event.x + 5, event.y + 5, outline="red",
+                                              width=0.5, fill="red")
+            calculate_distance()
 
 # Function to toggle measurement mode
 def toggle_measure_mode():
     global point1, point2, point1_oval, point2_oval, line, measure_mode
     measure_mode = not measure_mode
     if measure_mode:
-        measure_button.config(borderwidth = 3)
+        measure_button.config(borderwidth=3)
         canvas.bind("<Button-1>", set_point)
     else:
         # reset button and clear the previous points
-        measure_button.config(borderwidth = 0)
+        measure_button.config(borderwidth=0)
         canvas.unbind("<Button-1>")
         if point1:
             canvas.delete(point1_oval)
@@ -68,8 +72,9 @@ def toggle_measure_mode():
 
 # Function to open an image file
 def open_image():
-    global image_area_pixels
-    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.jpeg *.png *.bmp *.gif *.tiff")])
+    global image_area_pixels, image_loaded
+    file_path = filedialog.askopenfilename(
+        filetypes=[("Image files", "*.jpg *.jpeg *.png *.bmp *.gif *.tiff")])
     if file_path:
         image = Image.open(file_path)
         photo = ImageTk.PhotoImage(image)
@@ -81,7 +86,8 @@ def open_image():
             pixel_width = image.width
             pixel_height = image.height
             calc_image_size(pixel_width, pixel_height)
-            canvas.config(width=pixel_width, height=pixel_height+20)
+            canvas.config(width=pixel_width, height=pixel_height + 20)
+            image_loaded = True 
 
 # Create the main window
 root = tk.Tk()
@@ -100,7 +106,13 @@ open_button = tk.Button(button_frame, text="Open Image", command=open_image)
 open_button.grid(row=0, column=0)
 
 # Measure tool button
-measure_button = tk.Button(button_frame, text="Meaurement Tool", command=toggle_measure_mode)
+measure_icon = Image.open(r"C:\Users\youse\OneDrive\Desktop\School\Senior Spring\VS Code\Senior Design\measure_button.png")
+measure_icon = measure_icon.resize((20, 20))
+measure_icon = ImageTk.PhotoImage(measure_icon)
+
+# Measure tool button
+#measure_button = tk.Button(button_frame, text="Meaurement Tool", command=toggle_measure_mode)
+measure_button = tk.Button(button_frame, image=measure_icon, command=toggle_measure_mode)
 measure_button.grid(row=0, column=1)
 
 # Show Distance
